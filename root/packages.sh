a6f673a39bb1f89734ac3b6f6483d406cd46c0b1
Daniel Blanco <daniel@blancomartin.com.ar>
2016-05-14 18:57:00 -0300

    Update packages.sh

--- root/packages.sh
+++ root/packages.sh
@@ -2,7 +2,6 @@
 DEBIAN_FRONTEND=noninteractive apt-get -y install supervisor \
 openssh-server \
 apache2 \
-mysql-server-5.6 \
 php5 \
 libapache2-mod-php5 \
 php5-mysql \
@@ -10,6 +9,8 @@ php5-curl \
 php-pear \
 php5-imap \
 php5-memcache
+
+# mysql-server-5.6 \
 
 # apache2-suexec
 # libapache2-mod-fcgid
